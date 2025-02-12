094260435a499f9031912ab303c096d57d7576b9
QuocHuy3652 <quochuy3652@gmail.com>
2025-02-12 11:34:56 +0700

    init back-end

---
+++ back-end/movseek/src/main/java/com/huydevcorn/movseek/MovseekApplication.java
@@ -0,0 +1,13 @@
+package com.huydevcorn.movseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovseekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovseekApplication.class, args);
+	}
+
+}
