6bd859edcf360382a5d4fcc799e8f62af84da6fd
QuocHuy3652 <quochuy3652@gmail.com>
2025-02-21 02:45:26 +0700

    config swagger

--- back-end/movseek/src/main/java/com/huydevcorn/movseek/configuration/SwaggerConfig.java
+++ back-end/movseek/src/main/java/com/huydevcorn/movseek/configuration/SwaggerConfig.java
@@ -2,8 +2,6 @@ package com.huydevcorn.movseek.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +12,6 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info().title("API Documentation")
                         .version("1.0")
-                        .description("API Documentation"))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+                        .description("API Documentation"));
     }
 }
